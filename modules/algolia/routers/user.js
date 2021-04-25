4164404fb8d5385657b6f1cc4c7e201577de96c8
adrian <16654653+adaviloper@users.noreply.github.com>
2021-04-25 17:58:22 -0500

    Refactoring Proxy APIs - API Providers

--- modules/algolia/routers/user.js
+++ modules/algolia/routers/user.js
@@ -1,48 +1,17 @@
-import fetch from 'node-fetch';
-import {
-  getErrorResponse,
-  unWrap,
-} from '../../../utils/fetchUtils';
-import {
-  getHeaders,
-  sendJson,
-} from '../helpers';
+import { sendJson } from '../helpers';
 
-export default (algoliaConfig) => {
-  const headers = getHeaders(algoliaConfig)
+export default (apis) => {
   return async function getUserRoute(request, response, next) {
     const identity = request.identity;
-    const userData = await getUserById(identity);
+    const userData = await apis.user.getById(identity);
 
     if (userData.status === 200) {
       return sendJson(userData.json, response);
     }
 
-    createUser(identity);
-    sendJson(makeUserPayload(identity), response);
-    next();
-  }
-
-  async function createUser(identity) {
-    try {
-      return unWrap(await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${identity.id}`, {
-        headers,
-        method: 'PUT',
-        body: JSON.stringify(makeUserPayload(identity))
-      }));
-    } catch (error) {
-      return getErrorResponse(error);
-    }
-  }
-
-  async function getUserById(identity) {
-    try {
-      return unWrap(await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${identity.id}`, {
-        headers,
-      }));
-    } catch (error) {
-      return getErrorResponse(error);
-    }
+    const payload = makeUserPayload(identity);
+    apis.user.create(identity, payload);
+    sendJson(payload, response);
   }
 
   function makeUserPayload(identity) {
