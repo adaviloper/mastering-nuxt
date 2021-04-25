99093ff8021fccca96b083143f886b16f476dbcd
adrian <16654653+adaviloper@users.noreply.github.com>
2021-04-24 19:23:22 -0500

    Building an API Firewall (Part 2)

--- modules/auth.js
+++ modules/auth.js
@@ -1,4 +1,5 @@
 import cookie from 'cookie';
+import { OAuth2Client } from 'google-auth-library';
 
 export default function() {
   const authConfig = this.options.publicRuntimeConfig.auth;
@@ -6,10 +7,39 @@ export default function() {
   this.nuxt.hook('render:setupMiddleware', (app) => {
     app.use('/api', handler);
   });
-  function handler(request, response, next) {
+
+  async function handler(request, response, next) {
     const idToken = cookie.parse(request.headers.cookie)[authConfig.cookieName];
+    if (!idToken) {
+      return rejectHit(response);
+    }
+
     console.log('auth.js@handler:10', request.originalUrl);
     console.log(idToken);
+    const ticket = await getUser(idToken);
+    request.identity = {
+      id: ticket.sub,
+      email: ticket.email,
+      name: ticket.name,
+      image: ticket.picture,
+    };
     next()
+  }
+
+  async function getUser(idToken) {
+    const client = new OAuth2Client(authConfig.clientId);
+    try {
+      return (await client.verifyIdToken({
+        idToken,
+        audience: authConfig.clientId,
+      })).getPayload();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function rejectHit(response) {
+    response.statusCode = 401;
+    response.end();
   }
 }
